b38ef358175aa8091e912dde7c06f0d2db69f7ac
Koshin-Mohamed <koshin.moh@gmail.com>
2019-12-15 16:51:02 +0300

    change the name of the class to user and create a credential class.

---
+++ user.py
@@ -0,0 +1,15 @@
+class password:
+
+    """
+    A class to create new instances of a password
+    """
+
+    user_list = [] # Empty user list
+
+    def __init__(self,user_name):
+
+      # docstring removed for simplicity
+
+        self.user_name = user_name
+
+    user_list = [] # Empty user list
