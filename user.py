2e3d8e33343f78ceca85d87d41fd7cc9bf96ad9d
Koshin-Mohamed <koshin.moh@gmail.com>
2019-12-15 18:02:56 +0300

    add init function to create user, as well as a save and delete function.

--- user.py
+++ user.py
@@ -1,15 +1,42 @@
-class password:
+class User:
 
     """
-    A class to create new instances of a password
+    A class to create new instances of a user
     """
 
     user_list = [] # Empty user list
 
-    def __init__(self,user_name):
+    def __init__(self,account_type, email, username, password):
 
-      # docstring removed for simplicity
+        """[summary]
+        __init__ method that helps us define properties for our objects.
 
-        self.user_name = user_name
+        Arguments:
+            account_type: New user account type being saved.
+            email : New user email.
+            username: New user username.
+            password : New user password.
+        """
 
-    user_list = [] # Empty user list
+        self.account_type = account_type
+        self.email = email
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+
+    def delete_contact(self):
+
+        '''
+        delete_contact method deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self)
+
+
